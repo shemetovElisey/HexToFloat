381ffc9c06b4470cf71386bd543caa9f75297e04
Elisey <shemetov.elisey@gmail.com>
2020-12-17 01:20:12 +0700

    Initial Commit

---
+++ BinaryConverter/BinaryConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BinaryConverterApp.swift
+//  BinaryConverter
+//
+//  Created by Елисей on 17.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct BinaryConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
